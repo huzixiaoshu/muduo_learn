a89a6b8af595d70007d60192ee82a6fe78772a26
huzixiaoshu <1038794174@qq.com>
2021-06-05 19:19:28 +0800

    add time

---
+++ muduo/net/Poller.h
@@ -0,0 +1,68 @@
+// Author:Shuo Chen
+// learn : huzixiaohu
+// data:2021/06/05
+
+#ifndef MUDUO_NET_POLLER_H
+#define MUDUO_NET_POLLER_H
+
+#include <map>
+#include <vector>
+
+#include "muduo/base/Timestamp.h"
+#include "muduo/net/EventLoop.h"
+
+
+namespace muduo
+{
+namespace net
+{
+ 
+ class Channel;
+///
+/// Base class for IO Multiplexing
+///
+/// This class doesn't own the Channel objects.
+class Poller : noncopyable
+{
+public: 
+typedef std::vector<Channel*> ChannelList;
+
+Poller(EventLoop* loop);
+virtual ~Poller();
+
+  /// Polls the I/O events.
+  /// Must be called in the loop thread.
+
+virtual Timestamp poll(int timeoutMs, ChannelList* activeChannels)=0;
+
+/// Changes the interested I/O events;
+/// Must be called int the loop thread.
+
+virtual void updateChannel(Channel* Channel) =0;
+
+// Remove the channel, when it destructs.
+// Must be called in the loop thread.
+virtual void removeChannel(Channel* channel) = 0;
+
+virtual bool hasChannel(Channel* channel) const;
+
+static Poller* newDefaultPoller(EventLoop* loop);
+
+void assertInLoopThread() const
+{
+    ownerLoop_->assertInLoopThread();
+}
+
+protected:
+ typedef std::map<int, Channel*> ChannelMap;
+ ChannelMap channels_;
+
+private:
+
+ EventLoop* ownerLoop_;
+};
+
+} // namespace net
+} // namespace muduo
+
+#endif //MUDUO_NET_POLLER_H
